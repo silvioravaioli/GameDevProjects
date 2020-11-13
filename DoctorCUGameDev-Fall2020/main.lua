272f69811badc0ff163f8740739be66a5974efdd
ile123ile <bbravencofounder@gmail.com>
2020-11-13 18:34:45 -0500

    got started on love

--- DoctorCUGameDev-Fall2020/main.lua
+++ DoctorCUGameDev-Fall2020/main.lua
@@ -1,13 +1,17 @@
+gui = require('gui.lua')
+
 love.graphics.setDefaultFilter("nearest")
 
 function love.load()
-	print("loading game")
+	icon = love.graphics.newImage("redOrb.png")
 end
 
 function love.update(dt)
-	print("updating game")
+
 end
 
 function love.draw()
-	print("drawing images")
+	love.graphics.setBackgroundColor(1,1,1);
+	love.graphics.draw(icon, 50, 50, 0, 1, 1, 0)
+	gui:draw()
 end
--- conf.lua
+++
@@ -1,7 +0,0 @@
-function love.conf(t)
-	t.title = "Doctor Game"
-	t.window.width=1600
-	t.window.height=900
-	t.window.fullscreen = false
-	t.console=false
-end
