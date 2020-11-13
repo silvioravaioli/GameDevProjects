c46cf8bc33a6b3e034b37b5a09a05201b0aa570c
Da Hua Chen <dc2802@columbia.edu>
2020-11-13 18:15:01 -0500

    Add skeleton files

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+	t.title = "Doctor Game"
+	t.window.width=1600
+	t.window.height=900
+	t.window.fullscreen = false
+	t.console=false
+end
---
+++ main.lua
@@ -0,0 +1,13 @@
+love.graphics.setDefaultFilter("nearest")
+
+function love.load()
+	print("loading game")
+end
+
+function love.update(dt)
+	print("updating game")
+end
+
+function love.draw()
+	print("drawing images")
+end
