190e6309978cd5fac7e7950c1c6f5be749d9b8e2
ile123ile <bbravencofounder@gmail.com>
2020-11-13 19:55:23 -0500

    made hospital draw patients next to each other

--- DoctorCUGameDev-Fall2020/scripts/hospital.lua
+++ DoctorCUGameDev-Fall2020/scripts/hospital.lua
@@ -3,16 +3,20 @@ local hospital = {}
 hospital.patients = {}
 
 function hospital:load()
-	hospital.background = love.graphics.newImage("assets/redOrb.png");
-	hospital.patients[1] = love.graphics.newImage("assets/redOrb.png");
-	hospital.patients[2] = love.graphics.newImage("assets/redOrb.png");
-	hospital.patients[3] = love.graphics.newImage("assets/redOrb.png");
+	self.background = love.graphics.newImage("assets/redOrb.png");
+	self.patients[1] = love.graphics.newImage("assets/redOrb.png");
+	self.patients[2] = love.graphics.newImage("assets/blueOrb.png");
+	self.patients[3] = love.graphics.newImage("assets/greenOrb.png");
 	-- for patient in hospital.patients do
 	-- 	patient:load()
 	-- end
 end
 
 function hospital:draw()
+	for i = 1, #self.patients do
+		local patient = self.patients[i]
+		love.graphics.draw(patient, 100+patient:getWidth()*2*(i - 1), 500, 0, 1, 1)
+	end
 	-- love.graphics.draw()
 end
 
