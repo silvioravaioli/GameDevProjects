b11a20c4de17d7f274908d24fd88123a24b1f1ec
silvioravaioli <silvio.ravaioli@gmail.com>
2020-11-27 20:31:52 -0500

    Update disease_probability_matrix.lua

--- DoctorCUGameDev-Fall2020/files/disease_probability_matrix.lua
+++ DoctorCUGameDev-Fall2020/files/disease_probability_matrix.lua
@@ -10,3 +10,8 @@ disease_probability_matrix = [
 ]
 
 return disease_probability_matrix
+
+-- Table of disaese-symptoms probabilities
+-- Each row is a disease (1 to 8)
+-- Each column is a symptom (1 to 6)
+-- Each cell is the probability of the symptom, conditional on having the disease
