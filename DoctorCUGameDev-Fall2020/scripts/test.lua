111989e5c9a551def5f6c542065282c0e48872cc
silvioravaioli <silvio.ravaioli@gmail.com>
2020-12-04 19:40:08 -0500

    Update test.lua

--- DoctorCUGameDev-Fall2020/scripts/test.lua
+++ DoctorCUGameDev-Fall2020/scripts/test.lua
@@ -23,10 +23,26 @@ function test:toString()
 	return testStr
 end
 
-function test:mousepressed(x, y)
+
+function test:runTest(patient_number,test_number)
+	print('Run new test (patient number, test number)')
+	print(patient_number)
+	print(test_number)
+	testsAvailable = testsAvailable-1
+end
+
+function test:mousepressed(x, y)	
+	-- print coordinates
 	print('Mouse Pressed: coordinates:')
 	print(x,y)
+	-- print patient information (debug)
 	print(hospital.patient)
+	-- implement the test
+	print(testsAvailable)
+	if testsAvailable>0 then	
+		test:runTest(1,2)
+	end
+
 end
 	--print(ScaleX) -- you can print other stuff for debugging
 
