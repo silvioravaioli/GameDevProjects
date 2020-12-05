b912fb8e44c7890a5469ab9a300240822b38b7ff
silvioravaioli <silvio.ravaioli@gmail.com>
2020-12-04 19:03:47 -0500

    test

--- DoctorCUGameDev-Fall2020/scripts/test.lua
+++ DoctorCUGameDev-Fall2020/scripts/test.lua
@@ -1,6 +1,6 @@
-local tests = {}
+local test = {}
 
-Test = {
+testing = {
 	name = "Basic Test",            -- name of test
 	grade = "S",                    -- indicates quality of test
 	duration = 0,                   -- time required to run test
@@ -8,7 +8,7 @@ Test = {
 	symptoms = {}
 }
 
-function Test:new(name, grade, duration, icon)
+function test:new(name, grade, duration, icon)
 	setmetatable({}, test)
 	self.name = name
 	self.grade = grade
@@ -18,13 +18,27 @@ function Test:new(name, grade, duration, icon)
 	return self
 end
 
-function Test:toString()
+function test:toString()
 	testStr = string.format("Test: %s\nGrade:: %s\nTime Required:%s\n", self.name, self.grade, self.duration)
 	return testStr
 end
 
+function test:mousepressed(x, y)
+	print('Mouse Pressed: coordinates:')
+	print(x,y)
+	print(hospital.patient)
+end
+	--print(ScaleX) -- you can print other stuff for debugging
+
+	-- move to patient pages
+	--for i = 1, #self.patientBoxes do
+	--	xx = self.patientBoxes[i][1]
+	--	yy = self.patientBoxes[i][2]
+	--	if x >= xx and x <= xx + patientWidth and y >= yy and y <= yy + patientHeight then
+	--		page = "TEST"
+	--		currentPatient=i
+	--	end
+	--end
 
 
-
-
-return tests
+return test
